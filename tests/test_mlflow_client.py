65cf7b4bb0dc677ca97694a31da9c2c41a7c8b92
shizheng-rlfresh <shi.zheng.tfls@gmail.com>
2026-03-23 23:15:28 -0400

    update

--- tests/test_mlflow_client.py
+++
@@ -1,8 +0,0 @@
-"""test mlflow client"""
-
-from mlflow_monitor.mlflow_client import MonitorMLflowClient
-
-
-def test_mlflow_client():
-    client = MonitorMLflowClient()
-    assert client is not None
